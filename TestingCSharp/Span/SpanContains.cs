a9746fd2f4c66e441710a58740726df5cbd5567d
artem54325 <chilo997@mail.ru>
2024-07-20 13:05:19 +0300

    Изменил метод сравнения

--- TestingCSharp/Span/SpanContains.cs
+++ TestingCSharp/Span/SpanContains.cs
@@ -20,6 +20,19 @@ public class SpanContains
         }
     }
 
+    [Benchmark]// 5
+    public void StartChars()
+    {
+        var result = false;
+        if (TestText[0] == TestStartWithStr[0] && 
+            TestText[1] == TestStartWithStr[1] && 
+            TestText[2] == TestStartWithStr[2] &&
+            TestText[3] == TestStartWithStr[3])
+        {
+            result = true;
+        }
+    }
+
     [Benchmark] // 2
     public void Slice03()
     {
@@ -38,7 +51,7 @@ public class SpanContains
         var result = false;
         var span = TestText.AsSpan();
         if (span.Slice(0, 4).Equals(TestStartWithStr, 
-                StringComparison.CurrentCulture))
+                StringComparison.Ordinal))
         {
             result = true;
         }
@@ -50,7 +63,7 @@ public class SpanContains
         var result = false;
         var span = TestText.AsSpan();
         if (span.Slice(0, 4).Equals(TestStartWithChars, 
-                StringComparison.CurrentCulture))
+                StringComparison.Ordinal))
         {
             result = true;
         }
