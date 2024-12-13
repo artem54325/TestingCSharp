51405a7885bfce530328a1aed075d2d82ca0b3aa
artem54325 <chilo997@mail.ru>
2024-12-13 20:36:18 +0300

    Добавлено в интернирование строк

--- TestingCSharp/ВопросыСобеседования/ИнтернированиеСтрок.cs
+++ TestingCSharp/ВопросыСобеседования/ИнтернированиеСтрок.cs
@@ -7,8 +7,13 @@ public class ИнтернированиеСтрок
         string a = new string(new char[0]);
         string b = string.Empty;
         string c = Foo();
+        string d = "" + "";
+        string e = "";
+        
         Console.WriteLine(object.ReferenceEquals(a, b));
         Console.WriteLine(object.ReferenceEquals(a, c));
+        Console.WriteLine(object.ReferenceEquals(a, d));
+        Console.WriteLine(object.ReferenceEquals(a, e));
     }
 
     static string Foo()
